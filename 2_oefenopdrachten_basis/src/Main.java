715c90b55804c1068191c70608639c8a475f67d9
Anouk Driessen <info@anoukdriessen.nl>
2021-11-19 11:41:39 +0100

    setup basis oefenopgaven

---
+++ 2_oefenopdrachten_basis/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Beslissingsstructuren");
+
+        System.out.println("Methoden");
+
+        System.out.println("Arrays en Collecties");
+
+        System.out.println("Loops");
+    }
+}
